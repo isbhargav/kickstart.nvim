dfdf1ad815a2d7591b9eb4fb08d68b8ec8d90486
Bhargav <ladbhargav@yahoo.co.in>
2024-12-16 19:23:18 -0800

    gv command

--- lua/custom/plugins/gv.lua
+++ lua/custom/plugins/gv.lua
@@ -17,6 +17,31 @@ return {
         end)
       end,
     })
+
+    -- Diff between selected range of commits
+    vim.api.nvim_create_autocmd('FileType', {
+      group = vim.api.nvim_create_augroup('diffview-gv-v', { clear = true }),
+      pattern = 'GV',
+      callback = function()
+        vim.keymap.set('v', ',', function()
+          local line_number_start = vim.fn.line 'v' - 1
+          local line_number_end = vim.fn.line '.' - 1
+
+          local first_line = vim.api.nvim_buf_get_lines(0, line_number_start, line_number_start + 1, false)
+          local last_line = vim.api.nvim_buf_get_lines(0, line_number_end, line_number_end + 1, false)
+
+          if #first_line == 1 and #last_line == 1 then
+            local _, sha1, _ = string.match(first_line[1], '(^*%d%d%d%d--%d%d--%d%d%s)(%x+)(%s.*)')
+            local _, sha2, _ = string.match(last_line[1], '(^*%d%d%d%d--%d%d--%d%d%s)(%x+)(%s.*)')
+
+            if sha1 ~= nil and sha2 ~= nil then
+              local cmd = ':<BS><BS><BS><BS><BS>DiffviewOpen ' .. sha2 .. '..' .. sha1
+              vim.api.nvim_input(cmd)
+            end
+          end
+        end)
+      end,
+    })
   end,
   keys = {
     { '<leader>gv', '<cmd>GV<cr>', mode = 'n', desc = '[G]it [V]ertical Graph' },
